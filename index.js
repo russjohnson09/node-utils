20cb7c85ec21b7566a1bf8663c35b77e7497a13e
Russell Johnson <russell.johnson@livio.io>
2019-08-13 11:57:08 -0400

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+
+    hello: function()
+    {
+        console.log(`hello`)
+    }
+};
---
+++ test/test_spec.js
@@ -0,0 +1,12 @@
+const expect = require(`chai`).expect;
+
+
+describe(`test command helper`, function() {
+
+    it(`Command helper`, function() {
+
+        //TODO write tests
+        expect(null).to.not.be.null;
+    })
+
+});
